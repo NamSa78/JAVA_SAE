7129e0c85a53bc9278b0f43103c2dfa5f2ef0f73
Morel Mathis <mathismorel78@gmail.com>
2025-05-11 15:38:03 +0200

    création GraphMaze avec implémentation de graph

--- src-partie1/adaptator/GraphMaze.java
+++ src-partie1/adaptator/GraphMaze.java
@@ -3,8 +3,24 @@ package adaptator;
 import graph.Graph;
 import maze.Maze;
 
-import java.util.*;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+public class GraphMaze<T> implements Graph<T> {
 
-public class GraphMaze {
+    private final Maze<T> maze;
+
+    public GraphMaze(Maze<T> maze) {
+        this.maze = maze;
+    }
+
+    @Override
+    public List<Arc<T>> getSucc(T from) {
+        Set<T> opened = maze.openedNeighbours(from);
+        return opened.stream()
+                .map(to -> new Arc<>(1, to)) // tous les arcs ont un coût de 1
+                .collect(Collectors.toList());
+    }
 }
+
